41baace9e6217ffb08c38717dce58ce9190f4767
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-04-03 11:02:48 -0300

    Add conteudo em footer, main e header e estilização

--- crud/frontend/src/components/template/Footer.jsx
+++ crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react';
 
 export default props =>
     <footer className='footer'>
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Jefferson <span className="text-danger"> Formigoni</span></strong>
+        </span>
     </footer>
