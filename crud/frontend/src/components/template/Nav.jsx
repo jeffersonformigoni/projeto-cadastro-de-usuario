e4a9dff9d4ded766c028466ea834b4d6e4ea2c65
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-04-04 10:04:47 -0300

    Add conteudo em main nav e ssuasestilizaçoes

--- crud/frontend/src/components/template/Nav.jsx
+++ crud/frontend/src/components/template/Nav.jsx
@@ -1,7 +1,16 @@
-import './Nav.css';
-import React from 'react';
+import "./Nav.css";
+import React from "react";
 
-export default props =>
-    <aside className='menu-area'>
-        Navegação
-    </aside>
+export default (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+     {/*  Refatorar depois  */}
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i> Usúarios
+      </a>
+    </nav>
+  </aside>
+);
--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,18 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css'
-import './App.css';
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
+import React from "react";
 
-import Logo from '../components/template/Logo.'
-import Nav from '../components/template/Nav';
-import Main from '../components/template/Main';
-import Footer from '../components/template/Footer';
+import Logo from "../components/template/Logo.";
+import Nav from "../components/template/Nav";
+import Main from "../components/template/Main";
+import Footer from "../components/template/Footer";
 
-export default props =>
-    <div className='app'>
-        <Logo />
-        <Nav />
-        <Main icon='home' title='Início' 
-            subtitle='Segundo Projeto do capítulo de React.'/>
-        <Footer />
-    </div>
+export default (props) => (
+  <div className="app">
+    <Logo />
+    <Nav />
+    <Main
+      icon="home"
+      title="Início"
+      subtitle="Segundo Projeto do capítulo de React."
+    >
+      <div className="display-4">Bem Vindo!</div>
+      <hr />
+      <p>Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+    </Main>
+    <Footer />
+  </div>
+);
