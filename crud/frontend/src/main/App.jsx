c4eeb81f0ee618d25581f3a3539d6f91329f336e
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-04-04 10:10:49 -0300

    criado component Home

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -5,22 +5,14 @@ import React from "react";
 
 import Logo from "../components/template/Logo.";
 import Nav from "../components/template/Nav";
-import Main from "../components/template/Main";
+import Home from "../components/home/Home";
 import Footer from "../components/template/Footer";
 
 export default (props) => (
   <div className="app">
     <Logo />
     <Nav />
-    <Main
-      icon="home"
-      title="Início"
-      subtitle="Segundo Projeto do capítulo de React."
-    >
-      <div className="display-4">Bem Vindo!</div>
-      <hr />
-      <p>Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
-    </Main>
+    <Home />
     <Footer />
   </div>
 );
